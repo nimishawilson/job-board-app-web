2833a5b4a9cbc661d7f73826d6abb3786ee6dcc9
Nimisha Wilson <nimishawilson123@gmail.com>
2023-11-12 17:05:52 +0530

    style employer registration page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { EmployerRegistrationComponent } from './authentication/registration/employer-registration/employer-registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'employer-registration', component: EmployerRegistrationComponent}
+];
